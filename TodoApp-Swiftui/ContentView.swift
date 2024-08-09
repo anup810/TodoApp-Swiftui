0ac795f889218dbc5614726c8324e9d7848267d0
Anup Saud <anupsaud10@gmail.com>
2024-08-09 08:11:33 -0400

    Initial Commit

---
+++ TodoApp-Swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodoApp-Swiftui
+//
+//  Created by Anup Saud on 2024-08-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodoApp-Swiftui/TodoApp_SwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp_SwiftuiApp.swift
+//  TodoApp-Swiftui
+//
+//  Created by Anup Saud on 2024-08-09.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp_SwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
