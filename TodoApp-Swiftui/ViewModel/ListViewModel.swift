c35c062692757589814881fc3123366ab7371250
Anup Saud <anupsaud10@gmail.com>
2024-08-09 20:02:23 -0400

    Add functions to add, delete, update, move items, and load items from UserDefaults

--- TodoApp-Swiftui/ViewModel/ListViewModel.swift
+++ TodoApp-Swiftui/ViewModel/ListViewModel.swift
@@ -8,10 +8,26 @@
 import Foundation
 
 class ListViewModel: ObservableObject {
-   @Published var items: [ItemModel] = []
+    @Published var items: [ItemModel] = [] {
+        didSet{
+            saveItems()
+        }
+    }
+    init(){
+        getItems()
+    }
+    
     
     //must be unique
     let itemsKey: String = "items_List"
+    
+    func getItems(){
+        // get the data from userDefault
+        guard let data = UserDefaults.standard.data(forKey: itemsKey) else {return}
+        guard let saveData = try? JSONDecoder().decode([ItemModel].self, from: data) else {return}
+        
+        self.items = saveData
+    }
     
     func saveItems(){
         if let encodeData = try? JSONEncoder().encode(items){
@@ -19,6 +35,27 @@ class ListViewModel: ObservableObject {
         }
     }
     
+    
+    //func to add item
+    func addItem(title:String){
+        let newItem = ItemModel(Title: title, IsCompleted: false)
+        items.append(newItem)
+    }
+    //update
+    
+    func updateItem(item:ItemModel){
+        if let index = items.firstIndex(where: {  $0.id == item.id}){
+            items[index] = item.updateCompleted()
+        }
+    }
+    // delete item
+    func deleteItem(indexSet: IndexSet){
+        items.remove(atOffsets: indexSet)
+    }
+    //move items
+    func moveItems(from: IndexSet , to:Int){
+        items.move(fromOffsets: from, toOffset: to)
+    }
     
     
 }
